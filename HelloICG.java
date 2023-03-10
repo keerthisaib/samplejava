230f397a6711c8e7a8119825c39c792f470b94f6
keerthisaib <kb00781015@techmahindra.com>
2023-03-10 01:41:07 -0600

    1st version

--- HelloICG.java
+++ HelloICG.java
@@ -2,6 +2,7 @@ class HelloICG {
     public static void main(String[] args) {
         System.out.println("Hello Team");
 System.out.println("Hello ajk");
+System.out.println("Hello, "Hi");
 System.out.println("Devops team");        
 System.out.println("Hello, ICG!"); 
     }
